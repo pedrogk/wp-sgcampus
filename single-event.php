e408fc74b97632de9a612912887b7e2b6923375c
Pedro Galvan <pedro@sg.com.mx>
2014-09-03 16:59:06 -0500

    Updated single-event to show sidebar

--- single-event.php
+++ single-event.php
@@ -2,86 +2,109 @@
 /**
  * The template for displaying a single event
  *
- * Please note that since 1.7, this template is not used by default. You can edit the 'event details'
- * by using the event-meta-event-single.php template.
- *
- * Or you can edit the entire single event template by creating a single-event.php template
- * in your theme. You can use this template as a guide.
- *
- * For a list of available functions (outputting dates, venue details etc) see http://codex.wp-event-organiser.com/
- *
- ***************** NOTICE: *****************
- *  Do not make changes to this file. Any changes made to this file
- * will be overwritten if the plug-in is updated.
- *
- * To overwrite this template with your own, make a copy of it (with the same name)
- * in your theme directory. See http://docs.wp-event-organiser.com/theme-integration for more information
- *
- * WordPress will automatically prioritise the template in your theme directory.
- ***************** NOTICE: *****************
- *
  * @package Event Organiser (plug-in)
  * @since 1.0.0
  */
 
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+
+					// Previous/next post navigation.
+					twentyfourteen_post_nav();
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar( 'content' );
+get_sidebar();
+get_footer();
+
+------
+
 //Call the template header
 get_header(); ?>
 
-<div id="primary">
-	<div id="content" role="main">
+<div id="primary" class="content-area">
+  <div id="content" class="site-content" role="main">
+    <?php while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <?php twentyfourteen_post_thumbnail(); ?>
+      <header class="entry-header">
+        <h1 class="entry-title">
+          <?php the_title(); ?>
+        </h1>
+        <div class="entry-meta">
+          <?php
+        
+        if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+        ?>
+          <span class="comments-link">
+          <?php comments_popup_link( __( 'Leave a comment', 'sgcampus' ), __( '1 Comment', 'sgcampus' ), __( '% Comments', 'sgcampus' ) ); ?>
+          </span>
+          <?php
+        endif;
 
-		<?php while ( have_posts() ) : the_post(); ?>
+        edit_post_link( __( 'Edit', 'sgcampus' ), '<span class="edit-link">', '</span>' );
+        ?>
+        </div>
+        <!-- .entry-meta -->
+        <?php 
+        if ( is_single() ) : ?>
+        <!-- AddThis Button BEGIN -->
+        <div class="addthis_toolbox addthis_default_style" style="float: right;"> <a class="addthis_button_facebook_share" fb:share:layout="button_count" style="margin-right: 15px; line-height: 1;"></a> <a class="addthis_button_tweet" tw:via="sgcampus" style="margin-right:-10px;"></a> <a class="addthis_button_google_plusone" g:plusone:size="medium" style="margin-right:-15px;"></a> <a class="addthis_button_linkedin_counter" style="margin-right: 10px;"></a> <a class="addthis_counter addthis_pill_style"></a> </div>
+        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true, ui_language: "es" };</script> 
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5100717127708e7f"></script> 
+        <!-- AddThis Button END -->
+        <?php 
+        endif;
+        ?>
+      </header>
+      <!-- .entry-header -->
+      
+      <div class="entry-content single"> 
+        <!-- Get event information, see template: event-meta-event-single.php -->
+        <?php eo_get_template_part('event-meta','event-single'); ?>
+        
+        <!-- The content or the description of the event-->
+        <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sgcampus' ) ); ?>
+      </div>
+      <!-- .entry-content -->
+      
+      <?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+      
+    </article>
+    <!-- #post-<?php the_ID(); ?> --> 
+    
+    <!-- If comments are enabled, show them -->
+    <div class="comments-template">
+      <?php comments_template(); ?>
+    </div>
+    <?php endwhile; // end of the loop. ?>
+  </div>
+  <!-- #content --> 
+</div>
+<!-- #primary --> 
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<header class="entry-header">
-
-				<!-- Display event title -->
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-
-			</header><!-- .entry-header -->
-	
-			<div class="entry-content">
-				<!-- Get event information, see template: event-meta-event-single.php -->
-				<?php eo_get_template_part('event-meta','event-single'); ?>
-
-				<!-- The content or the description of the event-->
-				<?php the_content(); ?>
-			</div><!-- .entry-content -->
-
-			<footer class="entry-meta">
-			<?php
-				//Events have their own 'event-category' taxonomy. Get list of categories this event is in.
-				$categories_list = get_the_term_list( get_the_ID(), 'event-category', '', ', ',''); 
-
-				if ( '' != $categories_list ) {
-					$utility_text = __( 'This event was posted in %1$s by <a href="%5$s">%4$s</a>. Bookmark the <a href="%2$s" title="Permalink to %3$s" rel="bookmark">permalink</a>.', 'eventorganiser' );
-				} else {
-					$utility_text = __( 'This event was posted by <a href="%5$s">%4$s</a>. Bookmark the <a href="%2$s" title="Permalink to %3$s" rel="bookmark">permalink</a>.', 'eventorganiser' );
-				}
-				printf($utility_text,
-					$categories_list,
-					esc_url( get_permalink() ),
-					the_title_attribute( 'echo=0' ),
-					get_the_author(),
-					esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-				);
-			?>
-
-			<?php edit_post_link( __( 'Edit'), '<span class="edit-link">', '</span>' ); ?>
-			</footer><!-- .entry-meta -->
-
-			</article><!-- #post-<?php the_ID(); ?> -->
-
-			<!-- If comments are enabled, show them -->
-			<div class="comments-template">
-				<?php comments_template(); ?>
-			</div>				
-
-		<?php endwhile; // end of the loop. ?>
-
-	</div><!-- #content -->
-</div><!-- #primary -->
-
-<!-- Call template footer -->
-<?php get_footer(); ?>
+<?php
+get_sidebar( 'content' );
+get_sidebar();
+get_footer();
