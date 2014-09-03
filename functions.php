c2f17cee6ab257de1963c4da263c388894d58abb
Pedro Galvan <pedro@sg.com.mx>
2014-09-03 10:30:37 -0500

    Modified home loop in functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
 // Add your theme specific functions here
 
+function list_events_in_home_loop( $query ) {
+  if ( is_home() && $query->is_main_query() )
+    $query->set( 'post_type', array( 'post', 'event') );
+  return $query;
+}
+add_filter( 'pre_get_posts', 'list_events_in_home_loop' );
+
 ?>
